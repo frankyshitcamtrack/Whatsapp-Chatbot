9a56e26c8fdb5ce04ebd1db9378a6f0e3eb63412
Franky Kevin Shiti <Franky@camtrack.local>
2024-03-04 16:05:34 +0100

    add route file

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-
-
-const { onSendMessages, onVerification } = require("./src/controllers/whatsapp.controllers");
-
-
 const morgan = require('morgan')
 // Imports dependencies and set up http server
 const express = require("express");
+const whatsappRouter = require("./src/routes/whatsapp.route");
 
 
 const app = express();
@@ -16,8 +12,6 @@ app.use(express.json());
 
 
 // Accepts POST requests at /webhook endpoint
-app.post("/webhook", onSendMessages);
+app.post("/webhook", whatsappRouter);
 
-// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
-// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests 
-app.get("/webhook", onVerification);
+
