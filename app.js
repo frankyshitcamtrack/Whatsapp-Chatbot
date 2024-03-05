10cd0d7a2f3f25598f07ab3a7cccb09bd8119a76
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-03-05 16:42:23 +0100

    express json
    expressjson

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const morgan = require('morgan')
 
 const express = require("express");
 
-const body_parser = require("body-parser");
 
 const whatsappRouter = require("./src/routes/whatsapp.route");
 
 
 const app = express();
-
-app.use(body_parser.json());
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
