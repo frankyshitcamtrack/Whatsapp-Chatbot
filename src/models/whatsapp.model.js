f62bd546986f4db5c9947a45ffbeba67d1ca5f3c
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-03-05 18:49:14 +0100

    catch error
    catch error

--- src/models/whatsapp.model.js
+++ src/models/whatsapp.model.js
@@ -1,24 +1,24 @@
 const axios = require("axios");
-const {developement}= require("../config/whatsappApi");
+const { developement } = require("../config/whatsappApi");
 
 
 const token = developement.whatsapp_token;
 
-function sendMessages(phone_number_id,msg_body,from){
-    axios({
-        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-        url:
-          "https://graph.facebook.com/v12.0/" +
-          phone_number_id +
-          "/messages?access_token=" +
-          token,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: { body: "Ack: "+ msg_body},
-        },
-        headers: { "Content-Type": "application/json" },
-      });
+function sendMessages(phone_number_id, msg_body, from) {
+  axios.post("https://graph.facebook.com/v12.0/" +
+    phone_number_id +
+    "/messages?access_token=" +
+    token, {
+    messaging_product: "whatsapp",
+    to: from,
+    text: { body: "Ack: " + msg_body },
+  })
+    .then((response) => {
+      console.log(response);
+    }).catch(error=>{
+      console.log(error)
+    });
+  
 }
 
-module.exports={sendMessages}
+module.exports = { sendMessages }
