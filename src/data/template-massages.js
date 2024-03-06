cf19697d94e6c34e0a4d48eb7e4a6ca85ddef590
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-03-06 01:20:37 +0100

    update
    update

--- src/data/template-massages.js
+++ src/data/template-massages.js
@@ -53,7 +53,7 @@ const messageList ={
 
 const textMessage= {
     type:"text",
-    text:{preview_url: false,header:"Welcome to Camtrack to Camtrack ✨😃",body: "let us know how we can help you today by choosing from the folowing options:"+
+    text:{preview_url: false,body: " Welcome to Camtrack to Camtrack ✨😃 let us know how we can help you today by choosing from the folowing options:"+
              
     "-press 1 for vehicule🚗 Location " +
 
