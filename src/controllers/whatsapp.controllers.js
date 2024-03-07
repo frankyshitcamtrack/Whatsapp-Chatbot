33443f20e09fa69b8721a30bb4c1b5ff35cd849c
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-03-07 11:27:11 +0100

    new flow test
    new flow test

--- src/controllers/whatsapp.controllers.js
+++ src/controllers/whatsapp.controllers.js
@@ -24,7 +24,6 @@ async function onSendMessages(req, res) {
       //format phone number
       const phone = phoneFormat(from);
       if (req.body.entry[0].changes[0].value.messages[0].text.body === "1") {
-        onSendMessages 
         sendMessages(phone_number_id, phone,askImmatriculation.text);
           if(req.body.entry[0].changes[0].value.messages[0].text.body === "3307"){
             const message = await serverMessage();
