ae850aa47d77dfbbc0e98ad868e71efe2e2b47d3
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-03-13 11:24:47 +0100

    comment
    coomment log code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
 app.use(morgan('combined'));
 
-getPositionVehicule("CH057025");
+//getPositionVehicule("CH057025");
 
 app.use(cors());
 app.use(bodyParser.json());
