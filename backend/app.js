b330ec4ccb8973595b80655020144e24f7546e8c
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-09-03 12:25:17 +0100

    fix reload page

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,20 @@
-const morgan = require("morgan")
-
-const cors = require("cors")
-
-const path = require('path')
-
 const express = require("express");
+
+const path = require('path');
+
+const cors = require("cors");
+
+const morgan = require("morgan");
 
 const bodyParser = require("body-parser");
 
-const {v4 : uuidv4} = require('uuid');
-
-const id= uuidv4();
-
 const whatsappRouter = require("./src/routes/whatsapp.route");
 
-const api = require('./src/routes/api')
-
-const app = express();
-
-const {downloadVideo}= require('./src/utils/download');
+const api = require('./src/routes/api');
 
 const {SaveContact}=require('./src/utils/saveContacts')
+
+const app = express();
 
 app.use(morgan('combined'));
 
@@ -34,15 +28,7 @@ setInterval(()=>{
 
 SaveContact();
 
-app.use(express.static('public'))
-
-app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','index.html'))
-})
-
-
 //const url ='https://vss.camtrack.net:36301/fileSrv/fileDown.php?filePath=QzovUHJvZ3JhbSBGaWxlcyAoeDg2KS9Wc3NTZXJ2aWNlL3N0b3JhZ2UvYmluL3g2NC8uLi8uLi8uLi9odGRvY3MvdnNzRmlsZXMvYWxhcm1SZWNvcmQvMjAyNF8wNV8xMi9WT0tTMDAxMy9jaDAzXzIwMjQwNTEyXzA4MzkyOF8wODM5NDNfMTIyLmh3&token=40a7fbeb380a435c6e7f2053f070af16'
-
 
 //const downloadPath = path.resolve(`./public/assets/video/${id}.mp4`)
 
@@ -50,7 +36,18 @@ app.get('/*',(req,res)=>{
 app.use("/webhook", whatsappRouter);
 
 app.use("/api", api);
+
 //downloadVideo(url,downloadPath,'https://whattsapi.camtrack.net/');
+app.set('etag', 'strong'); 
+
+
+app.use(express.static(path.join(__dirname+'/public')));
+
+ 
+app.get('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+})
+
 
 module.exports = app
 
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = require('./app');
 
 //locale
 /* const PORT = process.env.PORT || 8000;
-const server = http.createServer(app);   */
+const server = http.createServer(app);    */
  
 
 
@@ -22,7 +22,7 @@ const options = {
   ca: fs.readFileSync('./ssl/camtracknet.ca-bundle'),
 };  
  
-const server = https.createServer(options,app );  
+const server = https.createServer(options,app ); 
 
 
 async function startServer(){
