0005c6291909d8dfc2d90cb79bb5c4197517f475
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-09-03 20:20:53 +0100

    responsive login

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = require('./app');
 
 //locale
 /* const PORT = process.env.PORT || 8000;
-const server = http.createServer(app);   */ 
+const server = http.createServer(app);    */
  
 
 
@@ -21,9 +21,9 @@ const options = {
   key: fs.readFileSync('./ssl/camtracknet.key'),
   cert: fs.readFileSync('./ssl/camtracknet.crt'),
   ca: fs.readFileSync('./ssl/camtracknet.ca-bundle'),
-};  
+};   
  
-const server = https.createServer(options,app ); 
+const server = https.createServer(options,app); 
  
 
 async function startServer(){
