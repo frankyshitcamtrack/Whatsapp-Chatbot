118e39417194cfe17f788c46ecd4205def64c07d
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-09-03 10:19:29 +0100

    change ressendfile

--- backend/app.js
+++ backend/app.js
@@ -34,11 +34,11 @@ setInterval(()=>{
 
 SaveContact();
 
-app.use(express.static(path.join(__dirname,'backend','public')));
+app.use(express.static(path.join(__dirname,'..','public')));
 
 
 app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'backend','public','index.html'))
+    res.sendFile('./index.html')
 })
 
 
