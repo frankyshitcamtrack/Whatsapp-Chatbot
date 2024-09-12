c774f41307e144d5a4944fb16cd5eb124dedca4b
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-09-12 10:26:44 +0100

    validate endpoint

--- backend/src/models/whatsapp.model.js
+++ backend/src/models/whatsapp.model.js
@@ -11,7 +11,7 @@ async function verifyContacts(phoneArr) {
    return axios({
       method: "POST", // Required, HTTP method, a string, e.g. POST, GET
       url:
-        "https://graph.facebook.com/v20.0/contacts",
+        "/v1/contacts",
       data:{
           "blocking": "wait",
           "contacts":  phoneArr,
