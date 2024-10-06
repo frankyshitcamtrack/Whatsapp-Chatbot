dda708adde449a0faf552bd286912600f7740c4c
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-10-06 17:34:47 +0100

    tst

--- Frontend/src/services/discussions.service.js
+++ Frontend/src/services/discussions.service.js
@@ -1,9 +1,14 @@
+import axios from "axios";
+
 import baseUrl from "../config"
+
+
 
 async function getDiscussions(){
   try{
-    const res = await fetch(`${baseUrl}/discussions`);
+    const res = await axios(`${baseUrl}/discussions`);
     const data = await res.json();
+    console.log(data);
     return data;
   }catch(error){
     console.log(error);
@@ -14,7 +19,7 @@ async function getDiscussions(){
 
 
 async function getDiscussionbyId(id){
-    const res = await fetch(`${baseUrl}/single-discussion/${id}`);
+    const res = await axios(`${baseUrl}/single-discussion/${id}`);
     const data =await res.json();
     return data;
 }
