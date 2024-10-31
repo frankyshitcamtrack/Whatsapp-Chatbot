3774ef9667819440b9ba89369d8bfc24cd107a87
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-10-31 10:31:32 +0100

    demmarrage suspect

--- backend/src/controllers/whatsapp-controller/wialonNotification.controller.js
+++ backend/src/controllers/whatsapp-controller/wialonNotification.controller.js
@@ -4,6 +4,8 @@ const { sendMessages } = require("../../models/whatsapp.model");
 const { getMessagesAndNumbers } = require("../../utils/getMessagesAndNumbers");
 const { formatArrPhones } = require("../../utils/fortmat-phone");
 const { developement } = require("../../config/whatsappApi");
+const { titleNotification } = require("../../data/constantes");
+
 const {
   insertContact,
   getWialonContactByID,
@@ -28,39 +30,52 @@ async function sendSimpleWialonNotification(number, mes) {
 //wiallon endpoints webhooks
 async function onSendWialonNotificationMultiple(req, res) {
   const wialonNotif = req.body;
-  sendSimpleWialonNotification("699113142", JSON.stringify(wialonNotif));
-  const getMessageAndExtractNumbers = getMessagesAndNumbers(wialonNotif);
-  const message = getMessageAndExtractNumbers.message;
-  const numbers = getMessageAndExtractNumbers.numbers;
 
-  //cron to save contacts in database;
-  if (numbers.length > 0) {
-    try {
-      const phones = formatArrPhones(numbers);
-      if (scheduleFunction === true) {
-        phones.map(async (item) => {
-          if (item) {
-            const id = uuidv4();
-            const contact = await getWialonContactByID(item);
-            if (contact && contact.length === 0) {
-              insertContact(id, item);
+  const wialonNotifContent = Object.keys(wialonNotif)[0].replace(/\s/g, " ");
+
+  console.log(wialonNotifContent);
+  //custom wiallon notifications
+  if (
+    wialonNotifContent.includes(titleNotification[0]) ||
+    wialonNotifContent.includes(titleNotification[1]) ||
+    wialonNotifContent.includes(titleNotification[2]) ||
+    wialonNotifContent.includes(titleNotification[3])
+  ) {
+    sendSimpleWialonNotification("699113142", JSON.stringify(wialonNotif));
+  } else {
+    const getMessageAndExtractNumbers = getMessagesAndNumbers(wialonNotif);
+    const message = getMessageAndExtractNumbers.message;
+    const numbers = getMessageAndExtractNumbers.numbers;
+
+    //cron to save contacts in database;
+    if (numbers.length > 0) {
+      try {
+        const phones = formatArrPhones(numbers);
+        if (scheduleFunction === true) {
+          phones.map(async (item) => {
+            if (item) {
+              const id = uuidv4();
+              const contact = await getWialonContactByID(item);
+              if (contact && contact.length === 0) {
+                insertContact(id, item);
+              }
             }
-          }
-        });
+          });
+        }
+        if (message) {
+          phones.map((item) => {
+            if (item) {
+              sendSimpleWialonNotification(item, message);
+            }
+          });
+          return res.status(201).json({ ok: true });
+        } else {
+          res.sendStatus(404);
+        }
+      } catch (error) {
+        console.error("error of: ", error);
+        return res.status(500).send("Post received, but we have an error!");
       }
-      if (message) {
-        phones.map((item) => {
-          if (item) {
-            sendSimpleWialonNotification(item, message);
-          }
-        });
-        return res.status(201).json({ ok: true });
-      } else {
-        res.sendStatus(404);
-      }
-    } catch (error) {
-      console.error("error of: ", error);
-      return res.status(500).send("Post received, but we have an error!");
     }
   }
 }
---
+++ backend/src/data/constantes.js
@@ -0,0 +1,8 @@
+const titleNotification = [
+  "Demarrage suspect",
+  "Entrée dans une zone dangereuse",
+  "Déconnexion du boîtier",
+  "Entrée dans un corridor dangereux",
+];
+
+module.exports = { titleNotification };
