c3bf2a62813d7d445f6c67c291265f9d66b73edc
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-12-09 07:52:42 +0100

    add discussions by status

--- backend/src/controllers/ymanebot-controllers/discussions.controllers.js
+++ backend/src/controllers/ymanebot-controllers/discussions.controllers.js
@@ -1,46 +1,68 @@
-const {getDiscussionById,getDiscussions,insertDiscussion}=require('../../models/ymanebot-models/discussions.model')
+const {
+  getDiscussionById,
+  getDiscussions,
+  insertDiscussion,
+  getDiscussionsByStatus,
+} = require('../../models/ymanebot-models/discussions.model');
 
-
-
-async function httpGetDiscussions(req,res){
-    try {
-        return res.status(200).json(await getDiscussions());
-    } catch (error) {
-        return res.status(500).json({
-            error: 'something went wrong with the server'
-        })
-    }
+async function httpGetDiscussions(req, res) {
+  try {
+    return res.status(200).json(await getDiscussions());
+  } catch (error) {
+    return res.status(500).json({
+      error: 'something went wrong with the server',
+    });
+  }
 }
 
-
-async function httpGetDiscussionById(req,res){
-   const id =`"${req.params.id}"`;
-    try {
-        return res.status(200).json(await getDiscussionById(id));
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            error: 'something went wrong with the server'
-        })
-    }
+async function httpGetDiscussionById(req, res) {
+  const id = `"${req.params.id}"`;
+  try {
+    return res.status(200).json(await getDiscussionById(id));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: 'something went wrong with the server',
+    });
+  }
 }
 
-
-async function httpInsertDiscussion(req,res){
-    const {id_discussion,message,numero,status,id_push_campaign} = req.body;
-    try {
-       const insert= await insertDiscussion(id_discussion,message,numero,status,id_push_campaign);
-       if(insert){
-        return res.status(201).json(insert);
-       }
-       
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            error: 'something went wrong with the server'
-        })
-    }
+async function httpGetDiscussionByStatus(req, res) {
+  const status = `"${req.params.status}"`;
+  try {
+    return res.status(200).json(await getDiscussionsByStatus(status));
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: 'something went wrong with the server',
+    });
+  }
 }
 
+async function httpInsertDiscussion(req, res) {
+  const { id_discussion, message, numero, status, id_push_campaign } = req.body;
+  try {
+    const insert = await insertDiscussion(
+      id_discussion,
+      message,
+      numero,
+      status,
+      id_push_campaign
+    );
+    if (insert) {
+      return res.status(201).json(insert);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: 'something went wrong with the server',
+    });
+  }
+}
 
-module.exports={httpInsertDiscussion,httpGetDiscussionById,httpGetDiscussions}
+module.exports = {
+  httpInsertDiscussion,
+  httpGetDiscussionById,
+  httpGetDiscussions,
+  httpGetDiscussionByStatus,
+};
--- backend/src/models/ymanebot-models/discussions.model.js
+++ backend/src/models/ymanebot-models/discussions.model.js
@@ -1,34 +1,45 @@
 const pool = require('../../config/db');
 
-
-async function getDiscussions(){
-    const result =await pool.query('SELECT * FROM discussions INNER JOIN pushs_campagnes ON discussions.id_push_campaign=pushs_campagnes.id WHERE discussions.isDelete=0');
-    return result[0];
+async function getDiscussions() {
+  const result = await pool.query(
+    'SELECT * FROM discussions INNER JOIN pushs_campagnes ON discussions.id_push_campaign=pushs_campagnes.id WHERE discussions.isDelete=0'
+  );
+  return result[0];
 }
 
-
-async function getDiscussionById(id){
-    const result = await pool.query(`SELECT * FROM discussions WHERE id_discussion=${id} AND isDelete=0`);
-    return result[0];
+async function getDiscussionById(id) {
+  const result = await pool.query(
+    `SELECT * FROM discussions WHERE id_discussion=${id} AND isDelete=0`
+  );
+  return result[0];
 }
 
-
-
-function insertDiscussion(id_discussion,numero,status,id_push_campaign) {
- 
- pool.query(
-        `INSERT INTO discussions SET id_discussion= ?, numero= ?, status= ?, id_push_campaign= ?`,
-        [id_discussion,numero,status,id_push_campaign]
-    );
+function insertDiscussion(id_discussion, numero, status, id_push_campaign) {
+  pool.query(
+    `INSERT INTO discussions SET id_discussion= ?, numero= ?, status= ?, id_push_campaign= ?`,
+    [id_discussion, numero, status, id_push_campaign]
+  );
 }
 
-function updateStatusDiscussion(id_discussion,status) {
-   const id= `"${id_discussion}"`;
-   const state = `"${status}"`
-   pool.query(`UPDATE discussions SET status=${state} WHERE id_discussion=${id}`)
+function updateStatusDiscussion(id_discussion, status) {
+  const id = `"${id_discussion}"`;
+  const state = `"${status}"`;
+  pool.query(
+    `UPDATE discussions SET status=${state} WHERE id_discussion=${id}`
+  );
 }
 
+async function getDiscussionsByStatus(status) {
+  const result = await pool.query(
+    `SELECT * FROM discussions WHERE status=${status} AND isDelete=0`
+  );
+  return result[0];
+}
 
-module.exports ={getDiscussions,getDiscussionById,insertDiscussion,updateStatusDiscussion}
-
-
+module.exports = {
+  getDiscussions,
+  getDiscussionById,
+  insertDiscussion,
+  updateStatusDiscussion,
+  getDiscussionsByStatus,
+};
