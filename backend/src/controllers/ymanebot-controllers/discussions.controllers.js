1fa3b133b665da5b40d479a56f30d6882addf59d
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-12-09 11:47:13 +0100

    dashoard

--- backend/src/controllers/ymanebot-controllers/discussions.controllers.js
+++ backend/src/controllers/ymanebot-controllers/discussions.controllers.js
@@ -88,42 +88,42 @@ async function httpCountDiscussionsBystatusAndTypeCampaign(req, res) {
   try {
     //get media messages by status
     const deliveredPushMediaMessages = await countSentMessageBytypeCampagne(
-      typeCampagnes[1].toString()
+      typeCampagnes[1]
     );
     const failedPushMediaMessages = await countFailedMessageBytypeCampagne(
-      typeCampagnes[1].toString()
+      typeCampagnes[1]
     );
     const pendingPushMediaMessages = await countPendingMessageBytypeCampagne(
-      typeCampagnes[1].toString()
+      typeCampagnes[1]
     );
     const deletedPushMediaMessages = await countDeletedMessageBytypeCampagne(
-      typeCampagnes[1].toString()
+      typeCampagnes[1]
     );
 
     //get maketing messages by status
     const deliveredPushMarketingMessages = await countSentMessageBytypeCampagne(
-      typeCampagnes[0].toString()
+      typeCampagnes[0]
     );
     const failedPushMarketingMessages = await countFailedMessageBytypeCampagne(
-      typeCampagnes[0].toString()
+      typeCampagnes[0]
     );
     const pendingPushMarketingMessages =
-      await countPendingMessageBytypeCampagne(typeCampagnes[0].toString());
+      await countPendingMessageBytypeCampagne(typeCampagnes[0]);
     const deletedPushMarketingMessages =
-      await countDeletedMessageBytypeCampagne(typeCampagnes[0].toString());
+      await countDeletedMessageBytypeCampagne(typeCampagnes[0]);
 
     //get newsletter messages by status
     const deliveredNewsLetterMessages = await countSentMessageBytypeCampagne(
-      typeCampagnes[2].toString()
+      typeCampagnes[2]
     );
     const failedNewsLetterMessages = await countFailedMessageBytypeCampagne(
-      typeCampagnes[2].toString()
+      typeCampagnes[2]
     );
     const pendingNewsLetterMessages = await countPendingMessageBytypeCampagne(
-      typeCampagnes[2].toString()
+      typeCampagnes[2]
     );
     const deletedNewsLetterMessages = await countDeletedMessageBytypeCampagne(
-      typeCampagnes[2].toString()
+      typeCampagnes[2]
     );
 
     if (
