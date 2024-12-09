ad6d7711c762673a6c4617f6721784edae6a9742
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-12-09 12:21:20 +0100

    final model dashboard

--- backend/src/models/ymanebot-models/discussions.model.js
+++ backend/src/models/ymanebot-models/discussions.model.js
@@ -73,7 +73,7 @@ async function countPendingMessageBytypeCampagne(typeCampagneName) {
 async function countDeletedMessageBytypeCampagne(typeCampagneName) {
   const TC = `"${typeCampagneName}"`;
   const result = await pool.query(
-    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE ( AND d.isDelete=0 AND t.isDelete=0 AND d.status='deleted' AND t.name=${TC})`
+    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE (d.isDelete=0 AND t.isDelete=0 AND d.status='deleted' AND t.name=${TC})`
   );
   return result[0];
 }
