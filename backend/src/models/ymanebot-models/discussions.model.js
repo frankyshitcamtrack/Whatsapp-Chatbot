4896d785f4e75d72fb350b07f2b5cca321b3b4cc
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2024-12-09 11:58:31 +0100

    discussion

--- backend/src/models/ymanebot-models/discussions.model.js
+++ backend/src/models/ymanebot-models/discussions.model.js
@@ -48,41 +48,34 @@ async function getDiscussionByTypeCampaignNameAndStatus(
 }
 
 async function countSentMessageBytypeCampagne(typeCampagneName) {
-  const ToString = typeCampagneName.toString();
-  console.log(typeCampagneName);
-  console.log(ToString);
   const result = await pool.query(
-    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE ((d.status='sent' OR d.status='read' OR d.status='delivered') AND d.isDelete=0 AND t.isDelete=0 AND t.name=${typeCampagneName})`
+    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE ((d.status='sent' OR d.status='read' OR d.status='delivered') AND d.isDelete=0 AND t.isDelete=0 AND t.name= ?`,
+    [typeCampagneName]
   );
   return result[0];
 }
 
 async function countFailedMessageBytypeCampagne(typeCampagneName) {
-  const ToString = typeCampagneName.toString();
-  console.log(typeCampagneName);
-  console.log(ToString);
   const result = await pool.query(
-    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE (d.isDelete=0 AND t.isDelete=0 AND d.status='failed' AND t.name=${typeCampagneName})`
+    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE (d.isDelete=0 AND t.isDelete=0 AND d.status='failed' AND t.name= ?`,
+    [typeCampagneName]
   );
+
   return result[0];
 }
 
 async function countPendingMessageBytypeCampagne(typeCampagneName) {
-  const ToString = typeCampagneName.toString();
-  console.log(typeCampagneName);
-  console.log(ToString);
   const result = await pool.query(
-    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE (d.isDelete=0 AND t.isDelete=0 AND d.status='pending' AND t.name=${typeCampagneName} )`
+    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE (d.isDelete=0 AND t.isDelete=0 AND d.status='pending' AND t.name= ?`,
+    [typeCampagneName]
   );
   return result[0];
 }
 
 async function countDeletedMessageBytypeCampagne(typeCampagneName) {
-  const ToString = typeCampagneName.toString();
-  console.log(typeCampagneName);
-  console.log(ToString);
   const result = await pool.query(
-    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE ( AND d.isDelete=0 AND t.isDelete=0 AND d.status='deleted' AND t.name=${typeCampagneName})`
+    `SELECT d.status, p.push_campagne_name,t.name FROM discussions as d,pushs_campagnes as p,types_campagnes AS t WHERE ( AND d.isDelete=0 AND t.isDelete=0 AND d.status='deleted' AND t.name= ?`,
+    [typeCampagneName]
   );
   return result[0];
 }
