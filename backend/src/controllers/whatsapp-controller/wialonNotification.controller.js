f2053ade81ae55b51a34dfb6e396c769df73fad7
frankyshitcamtrack <162108180+frankyshitcamtrack@users.noreply.github.com>
2025-02-13 09:47:26 +0100

    template notifications

--- backend/src/controllers/whatsapp-controller/wialonNotification.controller.js
+++ backend/src/controllers/whatsapp-controller/wialonNotification.controller.js
@@ -38,9 +38,42 @@ async function sendSimpleWialonNotification(number, mes) {
 async function onSendWialonNotificationMultiple(req, res) {
   const wialonNotif = req.body;
   const wialonNotifContent = Object.keys(wialonNotif)[0].replace(/\s/g, ' ');
+  const getMessageAndExtractNumbers = getMessagesAndNumbers(wialonNotif);
+  const message = getMessageAndExtractNumbers.message;
+  const numbers = getMessageAndExtractNumbers.numbers;
+
+  //cron to save contacts in database;
+  if (numbers.length > 0) {
+    try {
+      const phones = formatArrPhones(numbers);
+      if (message) {
+        phones.map(async (item) => {
+          if (item) {
+            const id = uuidv4();
+            const contact = await getWialonContactByID(item);
+            if (contact && contact.length === 0) {
+              await sendTemplateConsent(phoneID, item).then(() => {
+                setTimeout(() => {
+                  sendSimpleWialonNotification(item, message);
+                }, 10000);
+                insertContact(id, item);
+              });
+            } else {
+              sendSimpleWialonNotification(item, message);
+            }
+          }
+        });
+        return res.status(201).json({ ok: true });
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (error) {
+      console.error('error of: ', error);
+      return res.status(500).send('Post received, but we have an error!');
+    }
+  }
 
   //custom wiallon notifications
-
   if (
     wialonNotifContent.toLowerCase().includes(titleNotification[0]) ||
     wialonNotifContent.toLowerCase().includes(titleNotification[1]) ||
@@ -67,41 +100,6 @@ async function onSendWialonNotificationMultiple(req, res) {
           }, 100000);
         }
       });
-    }
-  } else {
-    const getMessageAndExtractNumbers = getMessagesAndNumbers(wialonNotif);
-    const message = getMessageAndExtractNumbers.message;
-    const numbers = getMessageAndExtractNumbers.numbers;
-
-    //cron to save contacts in database;
-    if (numbers.length > 0) {
-      try {
-        const phones = formatArrPhones(numbers);
-        if (message) {
-          phones.map(async (item) => {
-            if (item) {
-              const id = uuidv4();
-              const contact = await getWialonContactByID(item);
-              if (contact && contact.length === 0) {
-                await sendTemplateConsent(phoneID, item).then(() => {
-                  setTimeout(() => {
-                    sendSimpleWialonNotification(item, message);
-                  }, 10000);
-                  insertContact(id, item);
-                });
-              } else {
-                sendSimpleWialonNotification(item, message);
-              }
-            }
-          });
-          return res.status(201).json({ ok: true });
-        } else {
-          res.sendStatus(404);
-        }
-      } catch (error) {
-        console.error('error of: ', error);
-        return res.status(500).send('Post received, but we have an error!');
-      }
     }
   }
 }
